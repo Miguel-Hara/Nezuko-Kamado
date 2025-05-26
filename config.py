be8a58a83c3efc95ad986891fc64d7fee4634f88
ᴍɪɢᴜᴇʟ ᴏ’ʜᴀʀᴀ! <anmol0700k@gmail.com>
2025-05-26 13:23:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,3 @@
-# Don't Remove Credit @CodeFlix_Bots, @rohit_1888
-# Ask Doubt on telegram @CodeflixSupport
-#
-# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
-#
-# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
-# and is released under the MIT License.
-# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
-#
-# All rights reserved.
-#
 
 import os
 from os import environ,getenv
@@ -24,7 +13,7 @@ API_HASH = os.environ.get("API_HASH", "") #Your API Hash from my.telegram.org
 #--------------------------------------------
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002170811388")) #Your db channel Id
-OWNER = os.environ.get("OWNER", "sewxiy") # Owner username without @
+OWNER = os.environ.get("OWNER", "AS_Jinwoo") # Owner username without @
 OWNER_ID = int(os.environ.get("OWNER_ID", "7328629001")) # Owner id
 #--------------------------------------------
 PORT = os.environ.get("PORT", "8001")
@@ -33,51 +22,51 @@ DB_URI = os.environ.get("DATABASE_URL", "")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluooo")
 #--------------------------------------------
 FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "10"))  # 0 means no expiry
-BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/CodeflixSupport")
+BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/AS_Jinwoo")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "200"))
 #--------------------------------------------
-START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/ec17880d61180d3312d6a.jpg")
-FORCE_PIC = os.environ.get("FORCE_PIC", "https://telegra.ph/file/e292b12890b8b4b9dcbd1.jpg")
+START_PIC = os.environ.get("START_PIC", "https://livewallp.com/wp-content/uploads/2022/01/Nezuko-Kamado-Nezuko-Kamado-Kimetsu-no-Yaiba-1920x1080-60-fps.jpg")
+FORCE_PIC = os.environ.get("FORCE_PIC", "https://c.wallhere.com/photos/b2/a2/anime_anime_girls_digital_art_artwork_portrait_2D_Kimetsu_no_Yaiba_Kamado_Nezuko-1660179.jpg!d")
 
 #--------------------------------------------
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "linkshortify.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "arolinks.com")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "146ac91a8a224d0a5d7fe9fbf4050be3d8922a8b")
 TUT_VID = os.environ.get("TUT_VID","https://t.me/hwdownload/3")
 SHORT_MSG = "<b>⌯ Here is Your Download Link, Must Watch Tutorial Before Clicking On Download...</b>"
 
-SHORTENER_PIC = os.environ.get("SHORTENER_PIC", "https://telegra.ph/file/ec17880d61180d3312d6a.jpg")
+SHORTENER_PIC = os.environ.get("SHORTENER_PIC", "https://livewallp.com/wp-content/uploads/2022/01/Nezuko-Kamado-Nezuko-Kamado-Kimetsu-no-Yaiba-1920x1080-60-fps.jpg")
 #--------------------------------------------
 
 #--------------------------------------------
-HELP_TXT = "<b><blockquote>ᴛʜɪs ɪs ᴀɴ ғɪʟᴇ ᴛᴏ ʟɪɴᴋ ʙᴏᴛ ᴡᴏʀᴋ ғᴏʀ @Nova_Flix\n\n❏ ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅs\n├/start : sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ\n├/about : ᴏᴜʀ Iɴғᴏʀᴍᴀᴛɪᴏɴ\n└/help : ʜᴇʟᴘ ʀᴇʟᴀᴛᴇᴅ ʙᴏᴛ\n\n sɪᴍᴘʟʏ ᴄʟɪᴄᴋ ᴏɴ ʟɪɴᴋ ᴀɴᴅ sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ ᴊᴏɪɴ ʙᴏᴛʜ ᴄʜᴀɴɴᴇʟs ᴀɴᴅ ᴛʀʏ ᴀɢᴀɪɴ ᴛʜᴀᴛs ɪᴛ.....!\n\n ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ <a href=https://t.me/cosmic_freak>sᴜʙᴀʀᴜ</a></blockquote></b>"
-ABOUT_TXT = "<b><blockquote>◈ ᴄʀᴇᴀᴛᴏʀ: <a href=https://t.me/cosmic_freak>Yato</a>\n◈ ꜰᴏᴜɴᴅᴇʀ ᴏꜰ : <a href=https://t.me/otakuflix_network>ᴏᴛᴀᴋᴜғʟɪx ɴᴇᴛᴡᴏʀᴋ</a>\n◈ ᴀɴɪᴍᴇ ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/anime_cruise_netflix>ᴀɴɪᴍᴇ ᴄʀᴜɪsᴇ</a>\n◈ sᴇʀɪᴇs ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/webseries_flix>ᴡᴇʙsᴇʀɪᴇs ғʟɪx</a>\n◈ ᴀᴅᴜʟᴛ ᴍᴀɴʜᴡᴀ : <a href=https://t.me/pornhwa_flix>ᴘᴏʀɴʜᴡᴀs</a>\n◈ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href=https://t.me/cosmic_freak>subaru</a></blockquote></b>"
+HELP_TXT = "<b><blockquote>ʜᴇʏ ᴅᴀʀʟɪɴɢ~ ɪ'ᴍ ʏᴏᴜʀ ꜱᴇxʏ ꜰɪʟᴇ-ᴋᴏ ʙᴏᴛ 🍓 ᴀʟʟ ʀᴇᴀᴅʏ ᴛᴏ ꜱᴇʀᴠᴇ ʏᴏᴜ ɪɴ ᴍʏ ɴᴇᴢᴜᴋᴏ-ᴍᴏᴅᴇ 💖\n\n❥ ᴍʏ ᴄᴜᴛᴇ ᴄᴏᴍᴍᴀɴᴅꜱ:\n├ /start : ᴡᴀᴋᴇ ᴍᴇ ᴜᴘ, ʙᴀᴋᴀ 💢\n├ /about : ᴡᴀɴɴᴀ ᴋɴᴏᴡ ᴡʜᴏ ᴏᴡɴꜱ ᴛʜɪꜱ ʜᴏᴛɪᴇ? 👀\n└ /help : ʟᴇᴛ ᴍᴇ ᴄᴜᴅᴅʟᴇ ʏᴏᴜ ᴛʜʀᴏᴜɢʜ ᴍʏ ʙᴜᴛᴛᴏɴꜱ 💞\n\n❥ ᴄʟɪᴄᴋ ➤ sᴛᴀʀᴛ ᴍᴇ ➤ ᴊᴏɪɴ ᴍʏ ꜱᴇxʏ ʜᴜʙꜱ ➤ ᴛʀʏ ᴀɢᴀɪɴ 😚\n\nᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ ᴍʏ ʜᴀɴᴅꜱᴏᴍᴇ ᴋɪɴɢ 🖤 <a href=https://t.me/AS_Jinwoo>螿 Sᴜɴɢ•Jɪɴᴡᴏᴏ ࿐</a></blockquote></b>"
+ABOUT_TXT = "<b><blockquote>🌸 ᴍʏ ᴅᴀʀᴋ ꜱʜᴏᴜᴊᴏ ꜱᴏᴜʟ ɪꜱ ᴄᴏᴅᴇᴅ ʙʏ:\n\n◈ ᴄʀᴇᴀᴛᴏʀ: <a href=https://t.me/AS_Jinwoo>螿 Sᴜɴɢ•Jɪɴᴡᴏᴏ ࿐</a>\n◈ ꜰᴏᴜɴᴅᴇʀ ᴏꜰ: <a href=https://t.me/ABS_Updates>ᴬᴮˢ ᑌᴘᴅᴀᴛᴇ</a>\n◈ ʟᴇᴇᴄʜ ʜᴜʙ: <a href=https://t.me/leech_hub>ʟᴇᴇᴄʜ ᴇxᴛʀᴇᴍᴇ 💦</a>\n◈ ᴄɪɴᴇᴍᴀ ʜᴜʙ: <a href=https://t.me/Cinema_Hub_ABS>ᴀʙꜱ ᴍᴏᴠɪᴇ ꜱᴇʀɪᴇꜱ ᴄʟᴜʙ 🎬</a>\n◈ ᴅᴇᴠᴇʟᴏᴘᴇʀ: <a href=https://t.me/AS_Jinwoo>螿 Sᴜɴɢ•Jɪɴᴡᴏᴏ ࿐</a> 💻💘</blockquote></b>"
 #--------------------------------------------
 #--------------------------------------------
-START_MSG = os.environ.get("START_MESSAGE", "<b>ʜᴇʟʟᴏ {first}\n\n<blockquote> ɪ ᴀᴍ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ, ɪ ᴄᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ɪᴛ ғʀᴏᴍ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ.</blockquote></b>")
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "ʜᴇʟʟᴏ {first}\n\n<b>ᴊᴏɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ᴀɴᴅ ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ʀᴇʟᴏᴀᴅ button ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ʀᴇǫᴜᴇꜱᴛᴇᴅ ꜰɪʟᴇ.</b>")
+START_MSG = os.environ.get("START_MESSAGE", "<b>ʜᴇʏ {first}~ 🍓\n\n<blockquote>ɪ'ᴍ ʏᴏᴜʀ ꜱᴇxʏ ғɪʟᴇ-ᴋᴏ ʙᴏᴛ 💌 ɪ ᴄᴀɴ ꜱᴛᴏʀᴇ ʏᴏᴜʀ ɴᴀᴜɢʜᴛʏ... ɪ ᴍᴇᴀɴ ᴘʀɪᴠᴀᴛᴇ ꜰɪʟᴇꜱ ɪɴ ᴀ ꜱᴀꜰᴇ ᴄʜᴀɴɴᴇʟ 😚. ʏᴏᴜ ᴄᴀɴ ꜱʜᴀʀᴇ ᴛʜᴇᴍ ᴡɪᴛʜ ᴏᴛʜᴇʀꜱ ᴜꜱɪɴɢ ꜱᴘᴇᴄɪᴀʟ ʟɪɴᴋꜱ~ 🔗\n\nᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ ᴍʏ ʜᴏᴛ ᴅᴀᴅᴅʏ 🖤 <a href='https://t.me/AS_Jinwoo'>螿 Sᴜɴɢ•Jɪɴᴡᴏᴏ ࿐</a></blockquote></b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "ʜᴇʏ {first}~ 💌\n\n<b>ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ᴍʏ ʜᴏᴛ ʜᴜʙꜱ ꜰɪʀꜱᴛ~ 😚\nᴊᴜꜱᴛ ᴊᴏɪɴ ᴛʜᴇ ᴄʜᴀɴɴᴇʟꜱ ᴀɴᴅ ᴄʟɪᴄᴋ ᴏɴ ‘ʀᴇʟᴏᴀᴅ’ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ꜰɪʟᴇ 💦</b>")
 
-CMD_TXT = """<blockquote><b>» ᴀᴅᴍɪɴ ᴄᴏᴍᴍᴀɴᴅs:</b></blockquote>
+CMD_TXT = """<b><blockquote>💋 ʜᴇʏ ᴀᴅᴍɪɴ-ꜱᴀᴍᴀ~ ʜᴇʀᴇ'ꜱ ʏᴏᴜʀ ꜱᴘɪᴄʏ ᴄᴏᴍᴍᴀɴᴅ ᴍᴇɴᴜ 🍓</blockquote>
 
-<b>›› /dlt_time :</b> sᴇᴛ ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ ᴛɪᴍᴇ
-<b>›› /check_dlt_time :</b> ᴄʜᴇᴄᴋ ᴄᴜʀʀᴇɴᴛ ᴅᴇʟᴇᴛᴇ ᴛɪᴍᴇ
-<b>›› /dbroadcast :</b> ʙʀᴏᴀᴅᴄᴀsᴛ ᴅᴏᴄᴜᴍᴇɴᴛ / ᴠɪᴅᴇᴏ
-<b>›› /ban :</b> ʙᴀɴ ᴀ ᴜꜱᴇʀ
-<b>›› /unban :</b> ᴜɴʙᴀɴ ᴀ ᴜꜱᴇʀ
-<b>›› /banlist :</b> ɢᴇᴛ ʟɪsᴛ ᴏꜰ ʙᴀɴɴᴇᴅ ᴜꜱᴇʀs
-<b>›› /addchnl :</b> ᴀᴅᴅ ꜰᴏʀᴄᴇ sᴜʙ ᴄʜᴀɴɴᴇʟ
-<b>›› /delchnl :</b> ʀᴇᴍᴏᴠᴇ ꜰᴏʀᴄᴇ sᴜʙ ᴄʜᴀɴɴᴇʟ
-<b>›› /listchnl :</b> ᴠɪᴇᴡ ᴀᴅᴅᴇᴅ ᴄʜᴀɴɴᴇʟs
-<b>›› /fsub_mode :</b> ᴛᴏɢɢʟᴇ ꜰᴏʀᴄᴇ sᴜʙ ᴍᴏᴅᴇ
-<b>›› /pbroadcast :</b> sᴇɴᴅ ᴘʜᴏᴛᴏ ᴛᴏ ᴀʟʟ ᴜꜱᴇʀs
-<b>›› /add_admin :</b> ᴀᴅᴅ ᴀɴ ᴀᴅᴍɪɴ
-<b>›› /deladmin :</b> ʀᴇᴍᴏᴠᴇ ᴀɴ ᴀᴅᴍɪɴ
-<b>›› /admins :</b> ɢᴇᴛ ʟɪsᴛ ᴏꜰ ᴀᴅᴍɪɴs
-<b>›› /addpremium :</b> ᴀᴅᴅ ᴀ ᴘʀᴇᴍɪᴜᴍ ᴜꜱᴇʀ
-<b>›› /premium_users :</b> ʟɪsᴛ ᴀʟʟ ᴘʀᴇᴍɪᴜᴍ ᴜꜱᴇʀs
-<b>›› /remove_premium :</b> ʀᴇᴍᴏᴠᴇ ᴘʀᴇᴍɪᴜᴍ ꜰʀᴏᴍ ᴀ ᴜꜱᴇʀ
-<b>›› /myplan :</b> ᴄʜᴇᴄᴋ ʏᴏᴜʀ ᴘʀᴇᴍɪᴜᴍ sᴛᴀᴛᴜs
-<b>›› /count :</b> ᴄᴏᴜɴᴛ verifications
-"""
+<b>›› /dlt_time :</b> ꜱᴇᴛ ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ ᴛɪᴍᴇ 💣  
+<b>›› /check_dlt_time :</b> ꜱᴇᴇ ᴡʜᴇɴ ɪ ʟɪᴋᴇ ᴛᴏ ᴄʟᴇᴀɴ ᴜᴘ 🧼  
+<b>›› /dbroadcast :</b> ꜱᴇɴᴅ ᴠɪᴅ/ᴅᴏᴄ ᴛᴏ ᴇᴠᴇʀʏᴏɴᴇ 💌  
+<b>›› /ban :</b> ʙᴀɴ ᴀ ʙᴀᴋᴀ ᴜꜱᴇʀ 🖤  
+<b>›› /unban :</b> ꜱᴇᴛ ᴛʜᴇᴍ ꜰʀᴇᴇ... ᴍᴀʏʙᴇ 😼  
+<b>›› /banlist :</b> ᴄʜᴇᴄᴋ ᴍʏ ʙʟᴀᴄᴋʟɪꜱᴛ 🧾  
+<b>›› /addchnl :</b> ᴀᴅᴅ ꜰᴏʀᴄᴇ ꜱᴜʙ ʜᴜʙ 🌐  
+<b>›› /delchnl :</b> ᴘᴜʀɢᴇ ᴀ ᴄʜᴀɴɴᴇʟ ❌  
+<b>›› /listchnl :</b> ꜱʜᴏᴡ ᴀʟʟ ꜱᴜʙ ᴄʜᴀɴɴᴇʟꜱ 🔍  
+<b>›› /fsub_mode :</b> ᴛᴏɢɢʟᴇ ꜰᴏʀᴄᴇ ꜱᴜʙ ꜱᴘɪᴅᴇʀ-ᴍᴏᴅᴇ 🕷️  
+<b>›› /pbroadcast :</b> ꜱᴇɴᴅ ᴀ ʜᴏᴛ ᴘɪᴄ ᴛᴏ ᴀʟʟ 📸  
+<b>›› /add_admin :</b> ᴍᴀᴋᴇ ꜱᴏᴍᴇᴏɴᴇ ᴀ ʙᴏꜱꜱ 😈  
+<b>›› /deladmin :</b> ᴅᴇᴛʜʀᴏɴᴇ ᴀɴ ᴀᴅᴍɪɴ 🔪  
+<b>›› /admins :</b> ꜱᴇᴇ ᴀʟʟ ᴏᴜʀ ᴋɪɴɢꜱ & Qᴜᴇᴇɴꜱ 👑  
+<b>›› /addpremium :</b> ɢɪᴠᴇ ᴘʀᴇᴍɪᴜᴍ ꜱᴘᴇᴄɪᴀʟ ʟᴏᴠᴇ 💎  
+<b>›› /premium_users :</b> ʟɪꜱᴛ ᴍʏ ᴅᴀʀʟɪɴɢꜱ 💖  
+<b>›› /remove_premium :</b> ꜱᴛʀɪᴘ ᴛʜᴇᴍ ᴏꜰ ᴛʜᴇɪʀ ᴄʀᴏᴡɴ 👋  
+<b>›› /myplan :</b> ᴄʜᴇᴄᴋ ʏᴏᴜʀ ꜱᴇxʏ ꜱᴛᴀᴛᴜꜱ 💼  
+<b>›› /count :</b> ᴄᴏᴜɴᴛ ᴛʜᴇ ꜱɪɴꜱ... ɪ ᴍᴇᴀɴ ᴠᴇʀɪꜰɪᴄᴀᴛɪᴏɴꜱ 🔥  
+</b>"""
 #--------------------------------------------
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>• ʙʏ @nova_flix</b>") #set your Custom Caption here, Keep None for Disable Custom Caption
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False #set True if you want to prevent users from forwarding files from bot
@@ -90,10 +79,10 @@ USER_REPLY_TEXT = "ʙᴀᴋᴋᴀ ! ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ ꜱᴇɴᴘ
 
 #==========================(BUY PREMIUM)====================#
 
-OWNER_TAG = os.environ.get("OWNER_TAG", "rohit_1888")
-UPI_ID = os.environ.get("UPI_ID", "rohit23pnb@axl")
+OWNER_TAG = os.environ.get("OWNER_TAG", "@AS_Jinwoo")
+UPI_ID = os.environ.get("UPI_ID", "Ab-samad@fam")
 QR_PIC = os.environ.get("QR_PIC", "https://telegra.ph/file/3e83c69804826b3cba066-16cffa90cd682570da.jpg")
-SCREENSHOT_URL = os.environ.get("SCREENSHOT_URL", f"t.me/rohit_1888")
+SCREENSHOT_URL = os.environ.get("SCREENSHOT_URL", f"t.me/AS_Jinwoo")
 #--------------------------------------------
 #Time and its price
 #7 Days
