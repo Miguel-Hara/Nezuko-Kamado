e1ebd2167cc36e5fc4e1cbc99ec6692dbfba6159
ᴍɪɢᴜᴇʟ ᴏ’ʜᴀʀᴀ! <anmol0700k@gmail.com>
2025-05-26 13:27:39 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -202,7 +202,7 @@ async def start_command(client: Client, message: Message):
     else:
         reply_markup = InlineKeyboardMarkup(
             [
-                    [InlineKeyboardButton("• ᴍᴏʀᴇ ᴄʜᴀɴɴᴇʟs •", url="https://t.me/Nova_Flix/50")],
+                    [InlineKeyboardButton("• ᴄʜᴀɴɴᴇʟ •", url="https://t.me/ABS_Updates")],
 
     [
                     InlineKeyboardButton("• ᴀʙᴏᴜᴛ", callback_data = "about"),
